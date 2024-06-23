9fd041e82cd7bd6d1ef85945a772f24dfd523d5f
Aditya Tripathi <adityatripathi.at04@gmail.com>
2024-06-23 22:48:35 +0530

    chore: Update landing page styles and navbar layout

--- src/components/LandingNavbar.tsx
+++ src/components/LandingNavbar.tsx
@@ -2,33 +2,34 @@ import { Link } from "react-router-dom";
 
 const LandingNavbar = () => {
   return (
-    <nav className="flex justify-center items-center gap-5 mb-5">
+    <nav className="grid grid-cols-3 md:grid-cols-5 justify-items-center mb-5">
       <Link
-        className="text-2xl cursor-pointer flex justify-center items-center rounded-lg transition-all duration-150 hover:text-primary px-2 py-1"
+        className="md:text-2xl animated-underline cursor-pointer flex justify-center items-center rounded-lg transition-all duration-150 hover:text-primary px-2 py-1"
         to="/"
       >
         Home
       </Link>
       <Link
-        className="text-2xl cursor-pointer flex justify-center items-center rounded-lg transition-all duration-150 hover:text-primary px-2 py-1"
+        className="md:text-2xl animated-underline cursor-pointer flex justify-center items-center rounded-lg transition-all duration-150 hover:text-primary px-2 py-1"
         to="/about"
       >
         About
       </Link>
       <Link
-        className="text-2xl cursor-pointer flex justify-center items-center rounded-lg transition-all duration-150 hover:text-primary px-2 py-1"
+        className="md:text-2xl animated-underline cursor-pointer flex justify-center items-center rounded-lg transition-all duration-150 hover:text-primary px-2 py-1"
         to="/resume"
       >
         Resume
       </Link>
       <Link
-        className="text-2xl cursor-pointer flex justify-center items-center rounded-lg transition-all duration-150 hover:text-primary px-2 py-1"
+        className="md:text-2xl animated-underline cursor-pointer flex justify-center items-center rounded-lg transition-all duration-150 hover:text-primary px-2 py-1"
         to="/portfolio"
       >
         Portfolio
       </Link>
+      <div className="md:hidden"></div>
       <Link
-        className="text-2xl cursor-pointer flex justify-center items-center rounded-lg transition-all duration-150 hover:text-primary px-2 py-1"
+        className="md:text-2xl animated-underline cursor-pointer flex justify-center items-center rounded-lg transition-all duration-150 hover:text-primary px-2 py-1"
         to="/contact"
       >
         Contact
--- src/pages/FourZeroFour.tsx
+++ src/pages/FourZeroFour.tsx
@@ -1,14 +1,19 @@
+import { Link } from "react-router-dom";
+
 const FourZeroFour = () => {
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center">
-      <div className="flex flex-row items-baseline">
-        <h1 className="text-9xl font-black">404</h1>
-        <p className="text-2xl">Page not found.</p>
+    <div className="w-screen px-10 h-screen flex flex-col justify-center items-center">
+      <div className="flex flex-col md:flex-row md:items-baseline mb-5">
+        <h1 className="text-9xl font-black text-center md:text-left">404</h1>
+        <p className="text-2xl text-center md:text-left">Page not found.</p>
       </div>
-      <p>
+      <p className="text-center mb-5 md:text-left">
         The page you are looking for might have be on a vacation, let it have
         the time of it&apos;s life.
       </p>
+      <Link className="animated-underline" to="/">
+        Return home
+      </Link>
     </div>
   );
 };
