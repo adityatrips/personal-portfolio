b7ec358d5edba0737419cb96d1d5a3b6f5e6d4da
Aditya Tripathi <adityatripathi.at04@gmail.com>
2024-06-26 00:16:10 +0530

    added metadata to ezamizone page

--- src/pages/EzAmizonePrivacyPolicy.tsx
+++ src/pages/EzAmizonePrivacyPolicy.tsx
@@ -1,4 +1,23 @@
+import useMetadata from "../hooks/useMetadata";
+
 const EzAmizonePrivacyPolicy = () => {
+  useMetadata({
+    title: "Privacy Policy | EzAmizone",
+    author: "Aditya Tripathi",
+    description:
+      "Privacy Policy for EzAmizone, a learning management app for Amity University students.",
+    keywords: [
+      "privacy policy",
+      "ezamizone",
+      "amity university",
+      "learning management",
+      "google play",
+      "amizone",
+      "student app",
+    ],
+    url: "https://adityatripathi.vercel.app/ezamizone-privacy-policy",
+  });
+
   return (
     <div className="max-w-4xl mx-auto p-4">
       <h1 className="text-3xl font-bold text-center mb-4">
